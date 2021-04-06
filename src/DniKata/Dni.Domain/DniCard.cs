f2d9731f87489afec0a0ef425e0340c7fe32c79c
ambsenyestudi <adrifre7@hotmail.com>
2021-04-06 06:55:28 +0200

    Dni refactor

--- src/DniKata/Dni.Domain/DniCard.cs
+++ src/DniKata/Dni.Domain/DniCard.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Linq;
 
 namespace Dni.Domain
 {
     public record DniCard
     {
+        public const int CHARACTER_COUNT = 9;
         public DniCard(string rawDni)
         {
-            if(rawDni != "12345678A")
+            if(!IsRightLength(rawDni) || !EndInLetter(rawDni))
             {
                 throw new ArgumentException("");
             }
         }
+
+        private bool EndInLetter(string rawDni) =>
+            char.IsLetter(rawDni.Last());
+
+        private bool IsRightLength(string rawDni) =>
+            rawDni.Length == CHARACTER_COUNT;
     }
 }
